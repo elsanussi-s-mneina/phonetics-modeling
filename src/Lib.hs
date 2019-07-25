6a939f38820ba2ec0d51219eab4588f420d419c9
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2019-07-25 18:49:28 -0400

    Start defining a function for converting IPA symbols into its phonetic analysis.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
-    ( someFunc
+    ( analyzeIPA,
+      someFunc
     ) where
 
 data Phonet = Phonet { place :: Place   -- | Place of articulation
@@ -7,18 +8,56 @@ data Phonet = Phonet { place :: Place   -- | Place of articulation
                      , manner :: Manner -- | Manner of articulation
                      , airstream :: Airstream
                      }
+                     deriving (Eq, Show)
 
 data Place = Bilabial | LabioDental | Dental | Alveolar | PostAlveolar
            | Palatal  | Velar       | Glottal
+           deriving (Eq, Show)
 
 data Manner = Plosive | Fricative | Affricate | Nasal | Approximant
+              deriving (Eq, Show)
 
 data Airstream = PulmonicEgressive
+                 deriving (Eq, Show)
 
 data VocalFolds = Voiced | Voiceless
-
+                  deriving (Eq, Show)
 
 data PhonetInventory = PhonetInventory [Phonet]
+
+type IPAText = String
+  -- For storing text meant to be interpreted as International phonetic alphabet
+
+-- | This function will allow us to convert an IPA symbol
+-- | to its analyzed form (its phonetic features)
+analyzeIPA  :: IPAText -> Phonet
+analyzeIPA "p"  = Phonet  Bilabial Voiceless Plosive PulmonicEgressive
+analyzeIPA "b"  = Phonet  Bilabial Voiced Plosive PulmonicEgressive
+analyzeIPA "t"  = Phonet  Alveolar Voiceless Plosive PulmonicEgressive
+analyzeIPA "d"  = Phonet  Alveolar Voiced Plosive PulmonicEgressive
+analyzeIPA "k"  = Phonet  Velar Voiceless Plosive PulmonicEgressive
+analyzeIPA "g"  = Phonet  Velar Voiced Plosive PulmonicEgressive
+analyzeIPA "ʔ"  = Phonet  Glottal Voiceless Plosive PulmonicEgressive
+analyzeIPA "f"  = Phonet  LabioDental Voiceless Fricative PulmonicEgressive
+analyzeIPA "v"  = Phonet  LabioDental Voiced Fricative PulmonicEgressive
+analyzeIPA "ð"  = Phonet  Dental Voiced Fricative PulmonicEgressive,
+analyzeIPA "θ"  = Phonet  Dental Voiceless Fricative PulmonicEgressive,
+analyzeIPA "z"  = Phonet  Alveolar Voiced Fricative PulmonicEgressive,
+analyzeIPA "s"  = Phonet  Alveolar Voiceless Fricative PulmonicEgressive,
+analyzeIPA "ʒ"  = Phonet  PostAlveolar Voiced Fricative PulmonicEgressive,
+analyzeIPA "ʃ"  = Phonet  PostAlveolar Voiceless Fricative PulmonicEgressive,
+analyzeIPA "h"  = Phonet  Glottal Voiceless Fricative PulmonicEgressive,
+analyzeIPA "dʒ" = Phonet  PostAlveolar Voiced Affricate PulmonicEgressive,
+analyzeIPA "tʃ" = Phonet  PostAlveolar Voiceless Affricate PulmonicEgressive,
+analyzeIPA "m"  = Phonet  Bilabial Voiced Nasal PulmonicEgressive,
+analyzeIPA "n"  = Phonet  Alveolar Voiced Nasal PulmonicEgressive,
+analyzeIPA "ŋ"  = Phonet  Velar Voiced Nasal PulmonicEgressive,
+analyzeIPA "w"  = Phonet  Bilabial Voiced Approximant PulmonicEgressive,
+analyzeIPA "l"  = Phonet  Alveolar Voiced Approximant PulmonicEgressive,
+analyzeIPA "r"  = Phonet  PostAlveolar Voiced Approximant PulmonicEgressive,
+analyzeIPA "j"  = Phonet  Palatal Voiced Approximant PulmonicEgressive,
+analyzeIPA "w"  = Phonet  Velar Voiced Approximant PulmonicEgressive
+
 
 
 -- | This is a list of the sounds of English. Just the basic ones.
@@ -63,5 +102,7 @@ englishPhonetInventory = PhonetInventory
 
 
 
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Please read README.md file for instructions on how to use."
