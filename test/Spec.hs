47c51261a637cbfa55828d75c59c3214efdb1bb9
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-02-27 17:40:19 -0500

    Add a single unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+module Spec (main) where
+
+import Prelude (($), (++), IO, length, Maybe(Just), Bool(True, False))
+import Test.Hspec (describe, hspec, it, shouldBe, Spec)
+    
+import Lib(isGlide, analyzeIPA)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  do
+  hspec glideSpec
+
+
+
+glideSpec :: Spec
+glideSpec =
+  do
+  describe "isGlide" $ do
+    it "should return True when given [j] the voiced alveolar approximant " $
+          isGlide (analyzeIPA "j") `shouldBe` True
