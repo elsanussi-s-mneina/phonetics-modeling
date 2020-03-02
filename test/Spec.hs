ebbd857c9d55cb1e95b30bb26a4df846e68dc78c
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-02 15:32:05 -0500

    make test descriptions sound like what a linguist would say rather than a programmer

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,84 +15,84 @@ main = do
 
 glideSpec :: Spec
 glideSpec =
-  describe "isGlide" $ do
-    it "should return true when given [j] the voiced palatal approximant" $
+  describe "recognizing a glide" $ do
+    it "should be that: [j] the voiced palatal approximant is a glide." $
           isGlide (analyzeIPA "j") `shouldBe` True
-    it "should return false when given [ʝ] the voiced palatal fricative" $
+    it "should be that: [ʝ] the voiced palatal fricative is not a glide." $
           isGlide (analyzeIPA "ʝ") `shouldBe` False
-    it "should return true when given [w]" $
+    it "should be that: [w] is a glide." $
           isGlide (analyzeIPA "w") `shouldBe` True
-    it "should return false when given [c]" $
+    it "should be that: [c] is not a glide." $
           isGlide (analyzeIPA "c") `shouldBe` False
-    it "should return true when given [ɥ]" $
+    it "should be that: [ɥ] is a glide." $
           isGlide (analyzeIPA "ɥ") `shouldBe` True
 
 
 analyzeIPASpec :: Spec
 analyzeIPASpec = 
-    describe "analyzeIPA" $ do
-      it ("should return that [p] is a voiceless bilabial plosive consonant." ++ 
+    describe "analyzing place, voicing, manner, and airstream mechanism of sound represented by IPA symbols" $ do
+      it ("should be that: [p] is a voiceless bilabial plosive consonant" ++ 
           "with pulmonic egressive airstream mechanism.") $
             analyzeIPA "p" `shouldBe` Consonant Voiceless Bilabial Plosive PulmonicEgressive
-      it ("should return that [s] is a voiceless alveolar fricative consonant." ++
+      it ("should be that: [s] is a voiceless alveolar fricative consonant" ++
           "with pulmonic egressive airstream mechanism.") $
             analyzeIPA "s" `shouldBe` Consonant Voiceless Alveolar Fricative PulmonicEgressive
-      it ("should return that [b] is voiced.") $
+      it ("should be that: [b] is voiced.") $
             vocalFolds (analyzeIPA "b") `shouldBe` Voiced
-      it ("should return that [b] is bilabial.") $
+      it ("should be that: [b] is bilabial.") $
             place (analyzeIPA "b") `shouldBe` Bilabial
-      it ("should return that [b] is a plosive.") $
+      it ("should be that: [b] is a plosive.") $
             manner (analyzeIPA "b") `shouldBe` Plosive
-      it ("should return that [b] is pulmonic egressive.") $
+      it ("should be that: [b] is pulmonic egressive.") $
             airstream (analyzeIPA "b") `shouldBe` PulmonicEgressive
-      it ("should return that [t] is voiceless.") $
+      it ("should be that: [t] is voiceless.") $
             vocalFolds (analyzeIPA "t") `shouldBe` Voiceless
-      it ("should return that [t] is alveolar.") $
+      it ("should be that: [t] is alveolar.") $
             place (analyzeIPA "t") `shouldBe` Alveolar
-      it ("should return that [t] is a plosive.") $
+      it ("should be that: [t] is a plosive.") $
             manner (analyzeIPA "t") `shouldBe` Plosive
-      it ("should return that [t] is plumonic egressive.") $
+      it ("should be that: [t] is plumonic egressive.") $
             airstream (analyzeIPA "t") `shouldBe` PulmonicEgressive
-      it ("should return that [d] is voiced.") $
+      it ("should be that: [d] is voiced.") $
             vocalFolds (analyzeIPA "d") `shouldBe` Voiced
-      it ("should return that [d] is alveolar") $
+      it ("should be that: [d] is alveolar.") $
             place (analyzeIPA "d") `shouldBe` Alveolar
-      it ("should return that [d] is plosive") $
+      it ("should be that: [d] is plosive.") $
             manner (analyzeIPA "d") `shouldBe` Plosive
-      it ("should return that [d] is pulmonic egressive") $
+      it ("should be that: [d] is pulmonic egressive.") $
             airstream (analyzeIPA "d") `shouldBe` PulmonicEgressive
-      it ("should return that [ʈ] is voiceless") $
+      it ("should be that: [ʈ] is voiceless.") $
             vocalFolds (analyzeIPA "ʈ") `shouldBe` Voiceless
-      it ("should return that [ʈ] is retroflex") $
+      it ("should be that: [ʈ] is retroflex.") $
             place (analyzeIPA "ʈ") `shouldBe` Retroflex
-      it ("should return that [ʈ] is a plosive") $
+      it ("should be that: [ʈ] is a plosive.") $
             manner (analyzeIPA "ʈ") `shouldBe` Plosive
-      it ("should return that [ʈ] is pulmonic egressive") $
+      it ("should be that: [ʈ] is pulmonic egressive.") $
             airstream (analyzeIPA "ʈ") `shouldBe` PulmonicEgressive
-      it ("should return that [ɖ] is voiced") $
+      it ("should be that: [ɖ] is voiced.") $
             vocalFolds (analyzeIPA "ɖ") `shouldBe` Voiced
-      it ("should return that [ɖ] is retroflex") $
+      it ("should be that: [ɖ] is retroflex.") $
             place (analyzeIPA "ɖ") `shouldBe` Retroflex
-      it ("should return that [ɖ] is a plosive") $
+      it ("should be that: [ɖ] is a plosive.") $
             manner (analyzeIPA "ɖ") `shouldBe` Plosive
-      it ("should return that [ɖ] is pulmonic egressive") $
+      it ("should be that: [ɖ] is pulmonic egressive.") $
             airstream (analyzeIPA "ɖ") `shouldBe` PulmonicEgressive
 
 
 spirantizedIPASpec :: Spec
 spirantizedIPASpec = 
-    describe "spirantizedIPA" $ do
-      it ("should return that [β] is spirantized [b].") $
+    describe "spirantizing a sound (represented in IPA)" $ do
+      it ("should be that: [β] is spirantized [b].") $
             spirantizedIPA "b" `shouldBe` "β"
-      it ("should return that [ɸ] is spirantized [p].") $
+      it ("should be that: [ɸ] is spirantized [p].") $
             spirantizedIPA "p" `shouldBe` "ɸ"
-      it ("should return that [x] is spirantized [k].") $
+      it ("should be that: [x] is spirantized [k].") $
             spirantizedIPA "k" `shouldBe` "x"
 
 devoicedIPASpec :: Spec
 devoicedIPASpec =
-    describe "devoicedIPA" $ do
-      it ("should return that [p] is devoiced [b].") $
+    describe "devoicing a sound (represented in IPA)" $ do
+      it ("should be that: [p] is devoiced [b].") $
             devoicedIPA "b" `shouldBe` "p"
-      it ("should return that [n̥] is devoiced [n].") $
+      it ("should be that: [n̥] is devoiced [n].") $
             devoicedIPA "n" `shouldBe` "n̥"
