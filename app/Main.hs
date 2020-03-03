2ca27e5fd65595a14b67b7e93b93fec86265c2ee
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-02 23:53:06 -0500

    Add the very first specks of a functioning command line interface.
    Only one function is actually somewhat useful. The other one's still lack the ability to let the user specify the arguments (selection 2, and 3). This is a work in progress.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,32 @@
 module Main where
-
+import System.IO (hFlush, stdout)
 import Lib
+import InternationalPhoneticAlphabet (showIPA, voicedIPA, devoicedIPA)
+import English (englishPhonetInventory)
 
 main :: IO ()
 main = welcome
 
 
 welcome :: IO ()
-welcome = putStrLn "Please read README.md file for instructions on how to use."
+welcome = do
+    putStrLn "Please read README.md file for instructions on how to use."
+    putStr menu
+    hFlush stdout
+    selection <- getLine
+    putStrLn ("The user selected: " ++ selection ++ "\n")
+    putStrLn (case selection of
+                             "1" -> showIPA englishPhonetInventory
+                             "2" -> voicedIPA "s"
+                             "3" -> devoicedIPA "b"
+                             otherwise -> "Unrecognized selection. No action taken.")
+    putStrLn "\nProgram terminated normally.\n\n"
+
+                      
+
+menu :: String
+menu = "What do you want to accomplish?\n\n"
+        ++ "1) view the English phoneme inventory (as IPA graphemes).\n"
+        ++ "2) make a phoneme voiced.\n"
+        ++ "3) make a phoneme unvoiced.\n\n"
+        ++ "Enter the number representing your selection below, after the prompt, and press enter/return.\n\n\n(PROMPT:) "
