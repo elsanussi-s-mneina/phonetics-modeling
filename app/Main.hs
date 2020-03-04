3d698985e742698b2c0447fb79b7311f538a69ca
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-04 18:53:55 -0500

    Now possible for user to make an arbitrary phoneme voiced from the command line.
    Emphasis on "arbitrary".

--- app/Main.hs
+++ app/Main.hs
@@ -12,21 +12,35 @@ welcome :: IO ()
 welcome = do
     putStrLn "Please read README.md file for instructions on how to use."
     putStr menu
-    hFlush stdout
+    putPrompt
     selection <- getLine
     putStrLn ("The user selected: " ++ selection ++ "\n")
-    putStrLn (case selection of
-                             "1" -> showIPA englishPhonetInventory
-                             "2" -> voicedIPA "s"
-                             "3" -> devoicedIPA "b"
-                             otherwise -> "Unrecognized selection. No action taken.")
+    case selection of
+                   "1" -> putStrLn $ showIPA englishPhonetInventory
+                   "2" -> promptForPhonemeToVoice
+                   "3" -> putStrLn $ devoicedIPA "b"
+                   otherwise -> putStrLn $ "Unrecognized selection. No action taken."
     putStrLn "\nProgram terminated normally.\n\n"
 
-                      
+
+promptForPhonemeToVoice :: IO ()
+promptForPhonemeToVoice =  do
+    putStrLn "Enter the phoneme you would like to voice:"
+    putPrompt
+    phoneme <- getLine
+    putStrLn $ voicedIPA phoneme
+
+putPrompt :: IO ()
+putPrompt = do
+    putStr prompt
+    hFlush stdout
 
 menu :: String
 menu = "What do you want to accomplish?\n\n"
         ++ "1) view the English phoneme inventory (as IPA graphemes).\n"
         ++ "2) make a phoneme voiced.\n"
         ++ "3) make a phoneme unvoiced.\n\n"
-        ++ "Enter the number representing your selection below, after the prompt, and press enter/return.\n\n\n(PROMPT:) "
+        ++ "Enter the number representing your selection below, after the prompt, and press enter/return.\n\n\n"
+
+prompt :: String
+prompt = "(PROMPT:) "
