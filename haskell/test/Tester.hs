5a61eea2c792966fe61bb01c04367815c822ccf3
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-03-23 16:47:36 -0400

    remove color features form tester modules

--- haskell/test/Tester.hs
+++ haskell/test/Tester.hs
@@ -5,12 +5,8 @@ import Prelude ((++), IO, putStrLn, putStr, (==), Bool(True, False), String)
 
 conciseMode = True
 
-startRed = "\x1b[31m"
-
-startGreen = "\x1b[32m"
-endColor = "\x1b[0m" 
-concisePass = startGreen ++ "." ++ endColor
-conciseFail = startRed ++ "!" ++ endColor
+concisePass = "."
+conciseFail = "!"
 legend = "\n\nLegend:\n \t" ++ concisePass ++ "\t passed test\n\t" ++ conciseFail ++ "\t failed test"
 
 
@@ -21,5 +17,5 @@ printLegend = putStrLn legend
 runTest :: String -> Bool -> IO ()
 runTest description hasPassed =
   if hasPassed
-    then putStr (if conciseMode then "\x1b[32m.\x1b[0m" else "\x1b[32m should be that: \x1b[0m" ++ description ++ "\n")
-    else putStrLn ("! \x1b[35m SHOULD \x1b[31mBE THAT: \x1b[0m" ++ description ++ " FAILED!!!")
+    then putStr (if conciseMode then concisePass else "should be that:" ++ description ++ "\n")
+    else putStrLn (conciseFail  ++ "FAILED: " ++ description)
