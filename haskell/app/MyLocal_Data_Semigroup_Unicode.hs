5212829a0714d8e21fde02a9b050225bd7fb4a13
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-04-26 09:33:16 -0400

    Add comment explaining inspiration for module.

--- haskell/app/MyLocal_Data_Semigroup_Unicode.hs
+++ haskell/app/MyLocal_Data_Semigroup_Unicode.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE UnicodeSyntax #-}
+-- Base Unicode Symbols package lacked 
+-- a single character replacement for the <> operator,
+-- so I added it in this module. This module closely
+-- follows the template of the source code inside
+-- Base Unicode Symbols package.
+-- I made sure that the operator precedence
+-- of <> opeartor and its replacement are the same.
+
 
 module MyLocal_Data_Semigroup_Unicode ((◇))
   where
