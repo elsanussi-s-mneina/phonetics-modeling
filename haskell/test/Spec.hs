083f1f9bdd3fd01b41a155142e75387466e0bcea
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-08 13:41:14 -0400

    Add first unit tests (again)

--- haskell/test/Spec.hs
+++ haskell/test/Spec.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Spec(main) where
 
+import Data.Maybe (fromJust)
+import Test.Hspec (describe, hspec, it, shouldBe, Spec)
+
+import Lib_Functions (analyzeIPA, isGlide)
 
 
-main :: IO ()
-main = putStrLn "No tests implemented!"
+main = do
+  hspec glideSpec
+
+
+glideSpec :: Spec
+glideSpec =
+  describe "recognizing a glide" $ do
+    it "should be that: [j] the voiced palatal approximant is a glide." $
+          (isGlide . fromJust . analyzeIPA) "j" `shouldBe` True
+    it "should be that: [ʝ] the voiced palatal fricative is not a glide." $
+          (isGlide . fromJust . analyzeIPA) "ʝ" `shouldBe` False
+    it "should be that: [w] is a glide." $
+          (isGlide . fromJust . analyzeIPA) "w" `shouldBe` True
+    it "should be that: [c] is not a glide." $
+          (isGlide . fromJust . analyzeIPA) "c" `shouldBe` False
+    it "should be that: [ɥ] is a glide." $
+          (isGlide . fromJust . analyzeIPA) "ɥ" `shouldBe` True
+
+
+
