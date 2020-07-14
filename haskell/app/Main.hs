44378727e734aaf31aec2fdb80b33cf891b3b4e4
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-14 13:52:17 -0400

    Add inline documentation

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -20,10 +20,13 @@ putPrompt =
   putText prompt
   >> hFlush stdout
 
+-- | Print a blank line on the terminal.
 putBlankLine :: IO ()
 putBlankLine = putTextLn ""
 
-putBlankLines :: Int -> IO ()
+-- | Print a specified number of blank lines on the terminal.
+putBlankLines :: Int -- ^ number of lines
+              -> IO ()
 putBlankLines n = replicateM_ n putBlankLine
 
 promptForTextAndApply :: (Text -> Text) -> Text -> IO ()
@@ -32,31 +35,57 @@ promptForTextAndApply func instructions =
     >> putPrompt
     >> interact func
 
-interact :: (Text -> Text) -> IO ()
+-- | Given a function, asks the user for input,
+--   apply the function to the user input,
+--   and prints the result of the application
+--   of the function.
+interact :: (Text -> Text) -- ^ the function to apply to the user input
+         -> IO ()
 interact func =
   getLine
   >>= \userInput -> putTextLn (func userInput)
 
+-- | Ask the user for a phoneme.
+--   The user inputs a phoneme.
+--   Take the phoneme, and print the phoneme that is as similar
+--   to the original phoneme, but unvoiced.
 promptForPhonemeToDevoice :: IO ()
 promptForPhonemeToDevoice =
   promptForTextAndApply devoicedIPA phonemeToDevoiceMessage
 
+-- | Ask the user for a phoneme.
+--   The user inputs a phoneme.
+--   Take the phoneme, and print the phoneme that is as similar
+--   to the original phoneme, but voiced.
 promptForPhonemeToVoice :: IO ()
 promptForPhonemeToVoice =
   promptForTextAndApply voicedIPA phonemeToVoiceMessage
 
+-- | Ask the user for a phoneme.
+--   The user inputs a phoneme.
+--   Take the phoneme, and print the
+--   description of the phoneme.
 promptForPhonemeToDescribe :: IO ()
 promptForPhonemeToDescribe =
   promptForTextAndApply describeIPA phonemeToDescribeMessage
 
+-- | Ask the user for a phoneme.
+--   The user inputs a phoneme.
+--   Take the phoneme, calculate what
+--   the features of it are (according to Sound Patterns of English)
+--   and print those features.
 promptForPhonemeToCalculateSPEFeaturesFrom :: IO ()
 promptForPhonemeToCalculateSPEFeaturesFrom =
   promptForTextAndApply analyzeIPAToSPE phonemeToCalculateSPEMessage
 
+-- | Ask the user for IPA text which may contain multiple IPA characters
+--   and phonemes. Take that input, and print each phoneme on
+--   separate lines.
 promptForIPATextToSplit :: IO ()
 promptForIPATextToSplit =
   promptForTextAndApply ipaTextToPhonetListReport ipaTextToDivideMessage
 
+-- | This function is where the program starts running.
 main :: IO ()
 main =
   putTextLn pleaseReadReadmeMessage
@@ -74,7 +103,9 @@ handleSelection selection =
   >> putBlankLine
   >> respondToSelection selection
 
-respondToSelection :: Text -> IO ()
+-- | Start the appropriate action according to what the user already selected.
+respondToSelection :: Text -- ^ the text the user put in after being shown the menu
+                   -> IO ()
 respondToSelection selection
   | selection == userInputViewEnglishPhonemeInventory = putTextLn englishPhonetInventoryReport
   | selection == userInputMakeAPhonemeVoiced          = promptForPhonemeToVoice
@@ -84,9 +115,19 @@ respondToSelection selection
   | selection == userInputChunkIPAByPhoneme           = promptForIPATextToSplit
   | otherwise                                         = putTextLn unrecognizedSelectionMessage
 
-doAnalyzeIPA :: Text -> Text
+-- | Given an IPA transcription, return the
+--   name of the phoneme that IPA transcription describes.
+--   If the IPA transcription could not be named,
+--   return a message saying so.
+doAnalyzeIPA :: Text -- ^ text from the International Phonetic Alphabet
+             -> Text -- ^ the name of the phoneme,
+                     -- or a message saying the phoneme was not recognized
 doAnalyzeIPA x =
   maybe noAnalysisFoundMessage showPhonet (analyzeIPA x)
 
-doConstructIPA :: Phonet -> IO ()
+-- | Given a set of phoneme properties that describe a phoneme, like:
+--   voiced velar fricative pulmonic egressive,
+--   print the IPA transcription of it to the terminal.
+doConstructIPA :: Phonet  -- ^ A phonete to get the transcription of
+               -> IO ()
 doConstructIPA = putTextLn . constructIPA
