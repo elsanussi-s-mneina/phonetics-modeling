4cf23a2ff76efbc76338a88cbf7a120db8b68308
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-07-21 15:51:47 -0400

    Implement very basic input on a window.
    An input box, a button, and a label. When the button is clicked the phoneme is voiced.

--- haskell/app/MainWindow.hs
+++ haskell/app/MainWindow.hs
@@ -1,15 +1,48 @@
 {-# LANGUAGE OverloadedStrings #-}
 module MainWindow (startMainWindow) where
 
-import           Prelude       ()
+import           Prelude       (read)
 import           Relude
+
+import qualified Data.Text as T
 
 import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Types
 import Graphics.UI.FLTK.LowLevel.FLTKHS
 
+import           Lib           (voicedIPA)
+
+
+buttonCb :: Ref Input -> IO ()
+buttonCb i' = do
+  l' <- getValue i'
+  setLabel i' (voicedIPA l')
+
+ui :: IO ()
+ui = do
+ window <- windowNew
+           (Size (Width 215) (Height 200))
+           Nothing
+           Nothing
+ begin window
+ b' <- buttonNew
+        (Rectangle (Position (X 10) (Y 80)) (Size (Width 95) (Height 30)))
+        (Just "Make Phoneme Voiced")
+ setLabelsize b' (FontSize 10)
+
+ i' <- inputNew
+        (Rectangle (Position (X 50) (Y 30)) (Size (Width 30) (Height 30)))
+        (Just "")
+        (Just FlNormalInput)
+
+ setCallback i' buttonCb
+
+
+ end window
+ showWidget window
+
 startMainWindow :: IO ()
-startMainWindow =
-  do
-  putTextLn "A window should open. This has not been implemented yet."
-  return ()
+startMainWindow = ui >> FL.run >> FL.flush
+
+replMain :: IO ()
+replMain = ui >> FL.replRun
