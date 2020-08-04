1fd16805c16b9985cc65ff16b302ab5958977978
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-08-04 15:46:09 -0400

    implement digraph parsers
    using parser combinator approach

--- haskell/src/GraphemeGrammar2.hs
+++ haskell/src/GraphemeGrammar2.hs
@@ -24,18 +24,70 @@ We want something like this:
 
 BaseDigraph -> BaseCharacter | BaseCharacter TieCharacter BaseCharacter
 TieCharacter ->  ͜  | ͡
-BaseCharacter -> a | b | c | ɣ
+BaseCharacter -> a | b | c | t | s
 VoicingDiacritic -> ̥ | ̊ | ̯ |  ̆
 Diacritic -> ʰ
 SecondaryArticulationDiacritic ->  ʷ | ʲ | ˤ | ˠ
 -}
 
 baseCharacters :: [Char]
-baseCharacters = ['a', 'b', 'c']
+baseCharacters = ['a', 'b', 'c', 't', 's']
 
 
 secondaryArticulationDiacritics :: [Char]
 secondaryArticulationDiacritics = ['ʷ', 'ʲ', 'ˤ', 'ˠ']
+
+-- | Uses one parser on the text,
+--   then uses the next parser on the remaining
+--   text from the first parse.
+thenParser
+  :: (Text -> Maybe (Text, Text))
+  -> (Text -> Maybe (Text, Text))
+  -> Text
+  -> Maybe (Text, Text)
+thenParser firstParser secondParser text =
+  case firstParser text of
+    Nothing -> Nothing
+    Just (parsed, rest)
+            -> case secondParser rest of
+                 Nothing -> Nothing
+                 Just (parsed2, rest2) -> Just (parsed <> parsed2, rest2)
+
+
+-- | changes a parser by repeating it an indefinite number
+--   of times.
+--   So a parser that parses only "a", will parse "aaaaa".
+--   A parser that parses only "@", will parse "@@@@", "@@@@@" and
+--   so on.
+manyParser
+  :: (Text -> Maybe (Text, Text))
+  -> Text
+  -> Maybe (Text, Text)
+manyParser subParser text =
+  case subParser text of
+    Nothing -> Nothing
+    Just (parsed, rest)
+            -> case manyParser subParser rest of
+                  Nothing -> Just (parsed, rest)
+                  Just (parsed2, rest2) -> Just (parsed <> parsed2, rest2)
+
+-- | This implements the
+-- rule expressed in (BNF) grammar as:
+-- digraph -> baseCharacter tieBarCharacter baseCharacter
+digraphParser
+  :: Text
+  -> Maybe (Text, Text)
+digraphParser =
+  baseCharacterParser `thenParser` tieBarParser `thenParser` baseCharacterParser
+
+
+
+tieBarParser
+  :: Text
+  -> Maybe (Text, Text)
+tieBarParser = singleCharParser ['͜', '͡']
+
+
 
 phonemeParser
   :: Text
@@ -47,7 +99,7 @@ phonemeParser text =
     Just (parsed1, rest1)
       -> case secondaryArticulationDiacriticParser rest1 of
            Nothing
-             -> Nothing
+             -> Just (parsed1, rest1)
            Just (parsed2, rest2) -> Just (parsed1 <> parsed2, rest2)
 
 
--- haskell/test/GraphemeGrammar2Spec.hs
+++ haskell/test/GraphemeGrammar2Spec.hs
@@ -5,13 +5,25 @@ module GraphemeGrammar2Spec where
 import           Data.Maybe    (fromJust)
 import           Test.Hspec    (Spec, describe, hspec, it, shouldBe)
 import           Relude
-import           GraphemeGrammar2
+import           GraphemeGrammar2 ( singleCharParser
+                                  , secondaryArticulationDiacriticParser
+                                  , phonemeParser
+                                  , thenParser
+                                  , manyParser
+                                  , tieBarParser
+                                  , digraphParser)
 
 import qualified Data.Text as T
 
 
 graphemeGrammar2Spec = do
   hspec singleCharParserSpec
+  hspec secondaryArticulationDiacriticParserSpec
+  hspec phonemeParserSpec
+  hspec thenParserSpec
+  hspec manyParserSpec
+  hspec tieBarParserSpec
+  hspec digraphParserSpec
 
 singleCharParserSpec :: Spec
 singleCharParserSpec =
@@ -55,5 +67,68 @@ secondaryArticulationDiacriticParserSpec =
     it "should be that: parsing \"ˠc\" text succeeds with \"c\" remaining" $
       f "ˠc" `shouldBe` Just ("ˠ", "c")
     it "should be that: parsing \"ˤdefg\" text succeeds with \"defg\" remaining" $
-      f "ˤa" `shouldBe` Just ("ˤ", "defg")
+      f "ˤdefg" `shouldBe` Just ("ˤ", "defg")
+
+phonemeParserSpec :: Spec
+phonemeParserSpec =
+  let f = phonemeParser
+  in describe "phoneme parser" $ do
+    it "should be that: parsing empty text fails" $
+      isNothing (f "") `shouldBe` True
+    it "should be that: parsing \"a\" succeeds." $
+      f "a" `shouldBe` Just ("a", "")
+    it "should be that: parsing \"bc\" succeeds and consumes only one character" $
+      f "bc" `shouldBe` Just ("b", "c")
+    it "should be that: parsing a string containing only a superscript j fails" $
+      isNothing (f "ʲ") `shouldBe` True
+    it "should be that: parsing a string containing only a \"t\" followed by superscript j succeeds" $
+      f "tʲ" `shouldBe` Just("tʲ", "")
+
+thenParserSpec :: Spec
+thenParserSpec =
+  describe "then-parser" $ do
+    it "should be that: combining two single character parsers, parses two characters in same order" $ do
+      thenParser (singleCharParser ['a']) (singleCharParser ['b']) "abc" `shouldBe` Just("ab", "c")
+    it "should be that: combining two single character parsers, does not parse two characters in opposite order" $ do
+      thenParser (singleCharParser ['a']) (singleCharParser ['b']) "abc" `shouldBe` Just("ab", "c")
+    it "should be that: combining two single character parsers, parses two characters" $ do
+      thenParser (singleCharParser ['a']) (singleCharParser ['b']) "bac" `shouldBe` Nothing
+
+manyParserSpec :: Spec
+manyParserSpec =
+  describe "many parser" $ do
+    it "should be that: a many-parser on one character succeeds parsing when the character is the same." $ do
+      manyParser (singleCharParser ['a']) "a" `shouldBe` Just ("a", "")
+      manyParser (singleCharParser ['b']) "b" `shouldBe` Just ("b", "")
+      manyParser (singleCharParser ['3']) "3" `shouldBe` Just ("3", "")
+    it "should be that: a many-parser on one character fails when parsing a string that does not start with that character." $ do
+      manyParser (singleCharParser ['a']) "baa" `shouldBe` Nothing
+      manyParser (singleCharParser ['z']) "az" `shouldBe` Nothing
+    it "should be that: a many-parser on one characters succeeds on a string that contains only that character" $ do
+      manyParser (singleCharParser ['f']) "fff" `shouldBe` Just ("fff", "")
+      manyParser (singleCharParser ['f']) "fffa" `shouldBe` Just ("fff", "a")
+      manyParser (singleCharParser ['d']) "ddrst" `shouldBe` Just ("dd", "rst")
+
+tieBarParserSpec :: Spec
+tieBarParserSpec =
+  describe "tie-bar parser" $ do
+    it "parses the upper tie bar successfully"$ do
+      tieBarParser "͡" `shouldBe` Just ("͡", "")
+    it "parses the lower tie bar successfully"$ do
+      tieBarParser "͜" `shouldBe` Just ("͜", "")
+    it "parses only one tie bar at a time successfully"$ do
+      tieBarParser "͜͜" `shouldBe` Just ("͜", "͜")
+      -- Sorry this is hard to see, but there are two tie bar characters in the input.
+    it "does not parse characters that are not tie-bars"$ do
+      tieBarParser "abcdef" `shouldBe` Nothing
+
+digraphParserSpec :: Spec
+digraphParserSpec =
+  describe "digraph parser" $ do
+    it "parses t͜s successfully once" $ do
+      digraphParser "t͜s" `shouldBe` Just ("t͜s", "")
+    it "parses t͡s successfully once" $ do
+      digraphParser "t͡s" `shouldBe` Just ("t͡s", "")
+    it "fails to parse ts because it has no digraph" $ do
+      digraphParser "ts" `shouldBe` Nothing
 
